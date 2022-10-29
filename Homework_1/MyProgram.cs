7f6ce5a6ffd3de47e220f94ba794b0b4f9419767
Vladislav Borovikhin <50962824+Xanter410@users.noreply.github.com>
2022-10-29 22:03:35 +0700

    Выполнена домашняя работа №1

---
+++ Homework_1/MyProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homework_1
+{
+    public class MyProgram
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Боровихин Владислав Евгеньевич", "Vborovih@bk.ru", 22, 4.7f, 4.1f, 3.6f);
+            Console.Write(person.GetInfo());
+            Console.ReadKey();
+
+            Console.Write(person.GetInfoAvgScore());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework_1/Person.cs
@@ -0,0 +1,40 @@
+﻿namespace Homework_1
+{
+    public class Person
+    {
+        private string _fullName;
+        private string _email;
+        private int _age;
+        private float _scoreProgramming;
+        private float _scoreMath;
+        private float _scorePhysics;
+
+        public Person(string fullName, string email, int age, float pointProgramming, float pointMath, float pointPhysics)
+        {
+            _fullName = fullName;
+            _email = email;
+            _age = age;
+            _scoreProgramming = pointProgramming;
+            _scoreMath = pointMath;
+            _scorePhysics = pointPhysics;
+        }
+
+        public string GetInfo()
+        {
+            return $"Полное имя: {_fullName} \n" +
+                   $"Email: {_email} \n" +
+                   $"Возраст: {_age} \n" +
+                   $"Балл по программированию: {_scoreProgramming} \n" +
+                   $"Балл по математике: {_scoreMath} \n" +
+                   $"Балл по физике: {_scorePhysics} \n";
+        }
+
+        public string GetInfoAvgScore()
+        {
+            float sumScore = _scoreMath + _scoreMath + _scorePhysics;
+            float avgScore = sumScore / 3;
+
+            return $"Средний балл по всем предметам: {avgScore.ToString("#.##")} \n";
+        }
+    }
+}
