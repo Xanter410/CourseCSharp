ed8523ec7146d034478761d23066179ff6793afb
Vladislav Borovikhin <50962824+Xanter410@users.noreply.github.com>
2022-10-31 17:43:43 +0700

    Выполнена домашняя работа №4

---
+++ Homework_4/Homework_4_Task_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework_4_Task_1
+{
+    internal class Program
+    {
+        static public string[] SplitString(string input)
+        {
+            List<string> ResultList = new List<string>();
+            var begining = 0;
+            var len = input.Length;
+
+            for (var index = 0; index < len; index++)
+            {
+                if (input[index] == ' ')
+                {
+                    ResultList.Add(input.Substring(begining, index - begining));
+                    begining = index + 1;
+                }
+            }
+            ResultList.Add(input.Substring(begining));
+
+            return ResultList.ToArray();
+        }
+
+        static public void PrintArrayWords(string[] array)
+        {
+            foreach (string item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое предложение из нескольких слов: ");
+            string input = Console.ReadLine();
+
+            PrintArrayWords(SplitString(input));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework_4/Homework_4_Task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework_4_Task_2
+{
+    internal class Program
+    {
+        static public string[] SplitString(string input)
+        { 
+            return input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        static public string ReversWords(string input)
+        {
+            string[] words = SplitString(input);
+
+            return string.Join(" ", words.Reverse());
+        }
+
+        static private void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое предложение из нескольких слов: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(ReversWords(input));
+
+            Console.ReadKey();
+        }
+    }
+}
